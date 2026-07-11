806e9904364f936de75536cea6136518dc3fdae9
Lassoued Oussama <oussama.lassoued@isamm.u-manouba.tn>
2026-07-11 08:39:50 +0100

    add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -44,6 +44,7 @@ export default defineConfig(({mode}) => {
   );
 
   return {
+    base: mode === 'production' ? '/oussamalassoued/' : '/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
